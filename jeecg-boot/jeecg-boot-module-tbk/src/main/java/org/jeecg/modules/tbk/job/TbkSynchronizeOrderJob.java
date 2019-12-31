866664f02ca6e2a3a2261ac52ac453ea62faad4e
SwaggyLin <lmf951020@163.com>
2019-12-31 18:01:30 +0800

    同步订单任务

---
+++ jeecg-boot/jeecg-boot-module-tbk/src/main/java/org/jeecg/modules/tbk/job/TbkSynchronizeOrderJob.java
@@ -0,0 +1,21 @@
+package org.jeecg.modules.tbk.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * @Description
+ * @Author linminfeng
+ * @Date 2019-12-31 15:41
+ */
+public class TbkSynchronizeOrderJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
