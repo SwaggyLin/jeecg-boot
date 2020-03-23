0d55d718ceeffe74a2e48da406101c62e2389370
SwaggyLin <lmf951020@163.com>
2020-03-23 14:19:53 +0800

    代码优化

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/aspect/RelativeDataAspect.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/aspect/RelativeDataAspect.java
@@ -52,43 +52,18 @@ public class RelativeDataAspect {
     public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
     	long time1=System.currentTimeMillis();	
         Object result = pjp.proceed();
-        System.out.println("dcit start:"+JSONUtil.toJsonStr(result));
         long time2=System.currentTimeMillis();
         log.debug("获取JSON数据 耗时："+(time2-time1)+"ms");
         long start=System.currentTimeMillis();
-        this.parseDictText(result);
+        this.parseRelativeText(result);
         long end=System.currentTimeMillis();
         log.debug("解析注入JSON数据  耗时"+(end-start)+"ms");
-        System.out.println("dict end:"+JSONUtil.toJsonStr(result));
         return result;
     }
 
-    /**
-     * 本方法针对返回对象为Result 的IPage的分页列表数据进行动态字典注入
-     * 字典注入实现 通过对实体类添加注解@dict 来标识需要的字典内容,字典分为单字典code即可 ，table字典 code table text配合使用与原来jeecg的用法相同
-     * 示例为SysUser   字段为sex 添加了注解@Dict(dicCode = "sex") 会在字典服务立马查出来对应的text 然后在请求list的时候将这个字典text，已字段名称加_dictText形式返回到前端
-     * 例输入当前返回值的就会多出一个sex_dictText字段
-     * {
-     *      sex:1,
-     *      sex_dictText:"男"
-     * }
-     * 前端直接取值sext_dictText在table里面无需再进行前端的字典转换了
-     *  customRender:function (text) {
-     *               if(text==1){
-     *                 return "男";
-     *               }else if(text==2){
-     *                 return "女";
-     *               }else{
-     *                 return text;
-     *               }
-     *             }
-     *             目前vue是这么进行字典渲染到table上的多了就很麻烦了 这个直接在服务端渲染完成前端可以直接用
-     * @param result
-     */
-    private void parseDictText(Object result) {
+    private void parseRelativeText(Object result) {
         if (result instanceof Result) {
             if (((Result) result).getResult() instanceof IPage) {
-                Class<?> clazz=((IPage) ((Result) result).getResult()).getRecords().getClass();
                 List<Object> items = new ArrayList<>();
                 for (Object record : ((IPage) ((Result) result).getResult()).getRecords()) {
                     ObjectMapper mapper = new ObjectMapper();
@@ -102,38 +77,13 @@ public class RelativeDataAspect {
                     System.out.println(record.getClass());
 
                     JSONObject item = JSONObject.parseObject(json);
-                    //update-begin--Author:scott -- Date:20190603 ----for：解决继承实体字段无法翻译问题------
-                    //for (Field field : record.getClass().getDeclaredFields()) {
                     for (Field field : oConvertUtils.getAllFields(record)) {
-                    //update-end--Author:scott  -- Date:20190603 ----for：解决继承实体字段无法翻译问题------
-                        System.out.println(field.getName()+field.getAnnotation(Dict.class));
-
                         if (field.getAnnotation(Dict.class) != null) {
-                            String code = field.getAnnotation(Dict.class).dicCode();
-                            String text = field.getAnnotation(Dict.class).dicText();
-                            String table = field.getAnnotation(Dict.class).dictTable();
-                            String key = String.valueOf(item.get(field.getName()));
-
-                            //翻译字典值对应的txt
-                            String textValue = translateDictValue(code, text, table, key);
-
-                            log.debug(" 字典Val : "+ textValue);
-                            log.debug(" __翻译字典字段__ "+field.getName() + CommonConstant.DICT_TEXT_SUFFIX+"： "+ textValue);
-                            item.put(field.getName() + CommonConstant.DICT_TEXT_SUFFIX, textValue);
+                            translateDict(field,item);
                         }
                         if (field.getAnnotation(RelativeData.class) != null) {
-                            String tableName = field.getAnnotation(RelativeData.class).mainTable();
-                            String fieldName = field.getAnnotation(RelativeData.class).fieldName();
-                            String primaryKey = field.getAnnotation(RelativeData.class).primaryKey();
-                            String relativeKey = String.valueOf(item.get(field.getName()));
+                            translateRelativeData(field,item);
 
-                            //翻译
-                            String textValue = translateRelativeData(tableName, fieldName, primaryKey, relativeKey);
-
-
-                            log.debug(" 结果Val : "+ textValue);
-                            log.debug(" __翻译relativeData__ "+StrUtil.toCamelCase(fieldName)+"： "+ textValue);
-                            item.put(StrUtil.toCamelCase(fieldName), textValue);
                         }
                         //date类型默认转换string格式化日期
                         if (field.getType().getName().equals("java.util.Date")&&field.getAnnotation(JsonFormat.class)==null&&item.get(field.getName())!=null){
@@ -150,7 +100,31 @@ public class RelativeDataAspect {
     }
 
     /**
-          *  翻译字典文本
+     * 本方法针对返回对象为Result 的IPage的分页列表数据进行动态字典注入
+     * 字典注入实现 通过对实体类添加注解@dict 来标识需要的字典内容,字典分为单字典code即可 ，table字典 code table text配合使用与原来jeecg的用法相同
+     * 示例为SysUser   字段为sex 添加了注解@Dict(dicCode = "sex") 会在字典服务立马查出来对应的text 然后在请求list的时候将这个字典text，已字段名称加_dictText形式返回到前端
+     * 例输入当前返回值的就会多出一个sex_dictText字段
+     * {
+     *      sex:1,
+     *      sex_dictText:"男"
+     * }
+     */
+    private void translateDict(Field field, JSONObject item) {
+        String code = field.getAnnotation(Dict.class).dicCode();
+        String text = field.getAnnotation(Dict.class).dicText();
+        String table = field.getAnnotation(Dict.class).dictTable();
+        String key = String.valueOf(item.get(field.getName()));
+
+        //翻译字典值对应的txt
+        String textValue = translateDictValue(code, text, table, key);
+
+        log.debug(" 字典Val : "+ textValue);
+        log.debug(" __翻译字典字段__ "+field.getName() + CommonConstant.DICT_TEXT_SUFFIX+"： "+ textValue);
+        item.put(field.getName() + CommonConstant.DICT_TEXT_SUFFIX, textValue);
+    }
+
+    /**
+     *  翻译字典文本
      * @param code
      * @param text
      * @param table
@@ -174,7 +148,6 @@ public class RelativeDataAspect {
             }else {
                 tmpValue = dictService.queryDictTextByKey(code, k.trim());
             }
-
             if (tmpValue != null) {
                 if (!"".equals(textValue.toString())) {
                     textValue.append(",");
@@ -185,8 +158,38 @@ public class RelativeDataAspect {
         }
         return textValue.toString();
     }
-    private String translateRelativeData(String tableName, String fieldName, String primaryKey, String relativeKey) {
 
+    /**
+     * 本方法针对返回对象为Result 的IPage的分页列表数据进行动态关联数据注入
+     * 关联注入实现 通过对实体类添加注解@RelativeData 来标识需要的关联内容,mainTable主表,fieldName关联字段,primaryKey查询字段,默认为id
+     * 示例为EduClass   字段为classTeacherId 添加了注解	@RelativeData(mainTable = "edu_teacher",fieldName = "teacher_name") 会在关联表中立马查出来对应的teacher_name 然后在请求list的时候将这个以字段名称的驼峰形式返回到前端
+     * 例输入当前返回值的就会多出一个sex_dictText字段
+     * {
+     *      classTeacherId:36位id,
+     *      teacher_name:"天才"
+     * }
+     */
+    private void translateRelativeData(Field field, JSONObject item) {
+        String tableName = field.getAnnotation(RelativeData.class).mainTable();
+        String fieldName = field.getAnnotation(RelativeData.class).fieldName();
+        String primaryKey = field.getAnnotation(RelativeData.class).primaryKey();
+        String relativeKey = String.valueOf(item.get(field.getName()));
+        //翻译
+        String textValue = translateRelativeDataValue(tableName, fieldName, primaryKey, relativeKey);
+        log.debug(" 结果Val : "+ textValue);
+        log.debug(" __翻译relativeData__ "+StrUtil.toCamelCase(fieldName)+"： "+ textValue);
+        item.put(StrUtil.toCamelCase(fieldName), textValue);
+    }
+
+    /**
+     * 翻译关联数据文本
+     * @param tableName
+     * @param fieldName
+     * @param primaryKey
+     * @param relativeKey
+     * @return
+     */
+    private String translateRelativeDataValue(String tableName, String fieldName, String primaryKey, String relativeKey) {
         if (!StringUtils.isEmpty(relativeKey)){
             return sysBaseAPI.getRelativeData(tableName,fieldName,primaryKey,relativeKey);
         }else {
